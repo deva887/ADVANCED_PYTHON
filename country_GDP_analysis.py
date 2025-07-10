5e58df9ee39bbfd5861413636a2aa67a3c2a3d9a
Deva Kumar Sattala <devakumar@gmail.com>
2025-07-10 23:04:41 +0530

    Initial commit

--- country_GDP_analysis.py
+++ country_GDP_analysis.py
@@ -11,3 +11,32 @@ print(df.isnull().sum())
 print(df.head())
 print(df.tail())
 print(df.info())
+print(df.describe())#gives the numeric information only==descriptive statistics
+print(df['CountryName'])#to get specific attribute data
+df_cat=df[['CountryName','CountryCode','IncomeGroup']]
+print(df_cat)
+print(len(df.columns))
+print(df_cat.describe())
+print(len(df_cat.columns))
+df_num=df[['BirthRate','InternetUsers']]
+print(df_num)
+print(df.info())
+print(df_cat.info())
+print(df_num.info())
+print(df.describe())
+print(df.describe().transpose())
+print(df.describe().T)
+#renaming
+df.columns=['a','b','c','d','e']
+print(df)
+df.columns = ['countryName','CountryCode','BirthRate','InternetUsers','IncomeGroup']
+print(df)
+#subset
+print(df[['CountryCode','BirthRate','InternetUsers']][4:8])
+df['NewColumn']=df.BirthRate * df.InternetUsers
+print(df)
+print(len(df.columns))
+df=df.drop('NewColumn',axis=1)
+Filter_1 = df.InternetUsers<2
+Filter_2 = df.BirthRate>40
+print(df[Filter_1 & Filter_2])
